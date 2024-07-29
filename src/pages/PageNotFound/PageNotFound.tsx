27f38a5584bb3d492228cf80990ca5edb00bffa8
manc1n1 <mancinij1111@gmail.com>
2024-07-29 00:25:08 -0400

    Add pages

---
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,5 @@
+const PageNotFound: React.FC = () => {
+	return <h1>404</h1>;
+};
+
+export default PageNotFound;
---
+++ src/pages/Resume/Resume.tsx
@@ -0,0 +1,5 @@
+const Resume: React.FC = () => {
+	return <h1>Resume</h1>;
+};
+
+export default Resume;

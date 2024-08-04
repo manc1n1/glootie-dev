6f8eae4db1214bb5dc9d2494b4b56d5f882fd0c0
manc1n1 <mancinij1111@gmail.com>
2024-08-04 17:12:24 -0400

    Update Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,57 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Squeeze as Hamburger } from 'hamburger-react';
+import styles from './Header.module.css';
+
 const Header: React.FC = () => {
-	return <h1>Header</h1>;
+	const [isOpen, setOpen] = useState<boolean>(false);
+
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	};
+
+	const toggleMobileMenu = () => {
+		setOpen(!isOpen);
+	};
+
+	return (
+		<header className={styles.header}>
+			<div className={styles.headerBrand}>
+				<Link to="/" onClick={scrollToTop}>
+					glootie.dev
+				</Link>
+			</div>
+			<nav
+				className={`${styles.headerNav} ${
+					isOpen ? styles.headerNavOpen : ''
+				}`}
+			>
+				<Link to="/" onClick={toggleMobileMenu}>
+					Home
+				</Link>
+				<Link to="about" onClick={toggleMobileMenu}>
+					About
+				</Link>
+				<Link to="contact" onClick={toggleMobileMenu}>
+					Contact
+				</Link>
+				<Link to="resume" onClick={toggleMobileMenu}>
+					Resume
+				</Link>
+			</nav>
+			<div className={styles.headerToggle}>
+				<Hamburger
+					toggled={isOpen}
+					toggle={setOpen}
+					size={25}
+					label="Show menu"
+				/>
+			</div>
+		</header>
+	);
 };
 
 export default Header;

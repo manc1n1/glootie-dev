b1204464a37fc92e8505ae6931129478da37503f
manc1n1 <mancinij1111@gmail.com>
2024-08-04 17:10:10 -0400

    Update Layout

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import styles from './Layout.module.css';
 
 const Layout: React.FC = () => {
 	return (
 		<>
 			<Header />
-			<main>
+			<main className={styles.main}>
 				<Outlet />
 			</main>
 			<Footer />

1b582dfa47b40feb60907b9ba8d30ab992894d5a
manc1n1 <mancinij1111@gmail.com>
2024-08-05 19:48:29 -0400

    Update Contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,13 @@
+import ContactForm from '@/components/ContactForm';
+import styles from './Contact.module.css';
+
 const Contact: React.FC = () => {
-	return <h1>Contact</h1>;
+	return (
+		<section className={styles.container}>
+			<h1>Contact</h1>
+			<ContactForm />
+		</section>
+	);
 };
 
 export default Contact;
